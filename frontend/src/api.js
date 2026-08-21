daf31f5f5838fa48320d1c42d34075cf658b25e9
Amaan Saifi <asaifi0625@gmail.com>
2026-08-21 14:08:39 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // API BASE URL
 // ======================================================
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "/api"; ----------------------------------- <--
+const API_BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.DEV ? 'http://localhost:5001/api' : '/api');
 
 const API = axios.create({
   baseURL: API_BASE_URL,
